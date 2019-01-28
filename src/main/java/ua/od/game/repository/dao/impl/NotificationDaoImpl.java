be45226295c77538fb2d6ba3c78e0c412b92e422
fuasd <fuadaliyev1011@gmail.com>
2019-01-28 15:25:08 +0200

    Fixed NotificationDao

--- src/main/java/ua/od/game/repository/dao/impl/NotificationDaoImpl.java
+++ src/main/java/ua/od/game/repository/dao/impl/NotificationDaoImpl.java
@@ -15,23 +15,61 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class NotificationDaoImpl implements NotificationDao {
-    private final String GET_NOTIFICATION_LIST_QUERY = "SELECT n.id, n.name, n.description, n.show_from_scratch, n.resource_set_id, n.upgrade_set_id, n.building_set_id, rs.id, rs.resource_id, rs.amount, bs.id, bs.building_id, bs.amount, us.id , us.upgrade_id, us.amount FROM Notification n INNER JOIN Resource_Set rs on n.resource_set_id = rs.id INNER JOIN Upgrade_Set us on n.upgrade_set_id = us.id INNER JOIN Building_Set bs on n.building_set_id = bs.id;";
+    private final String GET_NOTIFICATION_LIST_QUERY =
+            "SELECT n.id, " + "" +
+                    "n.name,\n" +"" +
+                    "n.description,\n"+
+                    "n.show_from_scratch,\n" +
+                    "n.resource_set_id,\n" +
+                    "n.upgrade_set_id,\n" +
+                    "n.building_set_id,\n" +
+                    "rs.resource_id,\n" +
+                    "rs.amount rs_amount,\n" +
+                    "bs.building_id,\n" +
+                    "bs.amount bs_amount,\n" +
+                    "us.upgrade_id,\n" +
+                    "us.amount us_amount \n" +
+                    "FROM Notification n \n" +
+                    "INNER JOIN Resource_Set rs on n.resource_set_id = rs.id \n" +
+                    "INNER JOIN Upgrade_Set us on n.upgrade_set_id = us.id \n" +
+                    "INNER JOIN Building_Set bs on n.building_set_id = bs.id;";
+
+
+
 
 
     @Override
     public List<NotificationEntity> getAllNotificationList() {
         return SqlHelper.prepareStatement(GET_NOTIFICATION_LIST_QUERY, statementForNotificationList -> {
-            ResultSet notificationResultSet = statementForNotificationList.executeQuery();
+            ResultSet resultSet = statementForNotificationList.executeQuery();
             List<NotificationEntity> notifications = new LinkedList<>();
-            while (notificationResultSet.next()) {
+            while (resultSet.next()) {
                 notifications.add(new NotificationEntity() {{
-                    setId(notificationResultSet.getInt("id"));
-                    setName(notificationResultSet.getString("name"));
-                    setDescription(notificationResultSet.getString("description"));
-                    setShowFromScratch(notificationResultSet.getInt("show_from_scratch"));
-                    setResourceSetList(fetchResourceSetEntities(notificationResultSet));
-                    setBuildingSetList(fetchBuildingSetEntities(notificationResultSet));
-                    setUpgradeSetList(fetchUpgradeSetEntities(notificationResultSet));
+                    setId(resultSet.getInt("id"));
+                    setName(resultSet.getString("name"));
+                    setDescription(resultSet.getString("description"));
+                    setShowFromScratch(resultSet.getInt("show_from_scratch"));
+                    if (getBuildingSetList() == null)
+                        setBuildingSetList(new ArrayList<>());
+                    getBuildingSetList().add(new BuildingSetEntity() {{
+                        setId(resultSet.getInt("building_set_id"));
+                        setBuildingId(resultSet.getInt("building_id"));
+                        setAmount(resultSet.getFloat("bs_amount"));
+                    }});
+                    if (getResourceSetList() == null)
+                        setResourceSetList(new ArrayList<>());
+                    getResourceSetList().add(new ResourceSetEntity() {{
+                        setId(resultSet.getInt("resource_set_id"));
+                        setResourceId(resultSet.getInt("resource_id"));
+                        setAmount(resultSet.getFloat("rs_amount"));
+                    }});
+                    if (getUpgradeSetList () == null)
+                        setUpgradeSetList(new ArrayList<>());
+                    getUpgradeSetList().add(new UpgradeSetEntity() {{
+                        setId(resultSet.getInt("upgrade_set_id"));
+                        setUpgradeId(resultSet.getInt("upgrade_id"));
+                        setAmount(resultSet.getFloat("us_amount"));
+                    }});
                 }});
             }
             return notifications;
@@ -39,34 +77,5 @@ public class NotificationDaoImpl implements NotificationDao {
 
     }
 
-    private List<BuildingSetEntity> fetchBuildingSetEntities(ResultSet resultSet) throws SQLException {
-        List<BuildingSetEntity> buildingSetEntities = new ArrayList<> ();
-        buildingSetEntities.add(new BuildingSetEntity() {{
-            setId(resultSet.getInt("id"));
-            setBuildingId(resultSet.getInt("building_id"));
-            setAmount(resultSet.getFloat("amount"));
-        }});
-        return buildingSetEntities;
-    }
 
-    private List<ResourceSetEntity> fetchResourceSetEntities(ResultSet resultSet) throws SQLException {
-        List<ResourceSetEntity> resourceSetEntities = new ArrayList<> ();
-        resourceSetEntities.add(new ResourceSetEntity() {{
-            setId(resultSet.getInt("id"));
-            setResourceId(resultSet.getInt("resource_id"));
-            setAmount(resultSet.getFloat("amount"));
-        }});
-        return resourceSetEntities;
-    }
-
-    private List<UpgradeSetEntity> fetchUpgradeSetEntities(ResultSet resultSet) throws SQLException {
-        List<UpgradeSetEntity> upgradeSetEntities = new ArrayList<> ();
-        upgradeSetEntities.add(new UpgradeSetEntity() {{
-            setId(resultSet.getInt("id"));
-            setUpgradeId(resultSet.getInt("upgrade_id"));
-            setAmount(resultSet.getFloat("amount"));
-        }});
-
-        return upgradeSetEntities;
-    }
 }
--- src/test/java/ua/od/game/repository/dao/impl/NotificationDaoImplTest.java
+++ src/test/java/ua/od/game/repository/dao/impl/NotificationDaoImplTest.java
@@ -3,8 +3,11 @@ package ua.od.game.repository.dao.impl;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import ua.od.game.model.NotificationEntity;
 import ua.od.game.repository.dao.DbTest;
 import ua.od.game.repository.dao.NotificationDao;
+
+import java.util.List;
 
 public class NotificationDaoImplTest extends DbTest{
 
@@ -19,7 +22,18 @@ public class NotificationDaoImplTest extends DbTest{
     @Test
     public void getAllNotificationList() {
         Assert.assertFalse(notificationDao.getAllNotificationList().isEmpty());
-        System.out.println(notificationDao.getAllNotificationList());
+        List<NotificationEntity> notificationEntityList = notificationDao.getAllNotificationList ();
+        for(int i = 0;i < ((List) notificationEntityList).size ();i++) {
+            System.out.println (notificationEntityList.get(i).getId()+" ");
+            System.out.println (notificationEntityList.get(i).getName ()+" ");
+            System.out.println (notificationEntityList.get(i).getDescription ()+" ");
+            System.out.println (notificationEntityList.get(i).getShowFromScratch ()+" ");
+            System.out.println (notificationEntityList.get(i).getBuildingSetList ()+" ");
+            System.out.println (notificationEntityList.get(i).getResourceSetList ()+" ");
+            System.out.println (notificationEntityList.get(i).getUpgradeSetList ()+" ");
+        }
+
+
     }
 
 }
